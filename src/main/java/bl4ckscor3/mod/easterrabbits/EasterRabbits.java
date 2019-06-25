a37e0db45978e8daf9f844e82c0d5dc8b87904ba
bl4ckscor3 <donalddani9612@gmx.de>
2019-06-25 17:36:00 +0200

    update to 1.14.3

--- src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
+++ src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
@@ -3,9 +3,9 @@ package bl4ckscor3.mod.easterrabbits;
 import java.util.HashMap;
 import java.util.Random;
 
-import net.minecraft.entity.passive.EntityRabbit;
-import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,18 +17,18 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
 @EventBusSubscriber
 public class EasterRabbits
 {
-	public static final HashMap<EntityRabbit,Integer> TIME_UNTIL_NEXT_EGG = new HashMap<>();
+	public static final HashMap<RabbitEntity,Integer> TIME_UNTIL_NEXT_EGG = new HashMap<>();
 	public static final Random RAND = new Random();
 	private static final int FREQUENCY = 6000;
 
 	@SubscribeEvent
 	public static void onEntityJoinWorld(EntityJoinWorldEvent event)
 	{
-		if(event.getEntity() instanceof EntityRabbit)
+		if(event.getEntity() instanceof RabbitEntity)
 		{
 			if(!TIME_UNTIL_NEXT_EGG.containsKey(event.getEntity()))
 			{
-				TIME_UNTIL_NEXT_EGG.put((EntityRabbit)event.getEntity(), RAND.nextInt(FREQUENCY) + FREQUENCY);
+				TIME_UNTIL_NEXT_EGG.put((RabbitEntity)event.getEntity(), RAND.nextInt(FREQUENCY) + FREQUENCY);
 				return;
 			}
 		}
@@ -37,14 +37,14 @@ public class EasterRabbits
 	@SubscribeEvent
 	public static void onLivingDeath(LivingDeathEvent event)
 	{
-		if(event.getEntity() instanceof EntityRabbit)
+		if(event.getEntity() instanceof RabbitEntity)
 			TIME_UNTIL_NEXT_EGG.remove(event.getEntity());
 	}
 
 	@SubscribeEvent
 	public static void onServerTick(ServerTickEvent event)
 	{
-		for(EntityRabbit rabbit : TIME_UNTIL_NEXT_EGG.keySet())
+		for(RabbitEntity rabbit : TIME_UNTIL_NEXT_EGG.keySet())
 		{
 			TIME_UNTIL_NEXT_EGG.put(rabbit, TIME_UNTIL_NEXT_EGG.get(rabbit) - 1);
 
