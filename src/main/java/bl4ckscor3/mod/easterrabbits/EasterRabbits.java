7f347f10dfc8c43576163035c8d10d3b8c0adab5
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-01 10:33:45 +0200

    update class names

--- src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
+++ src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
@@ -3,9 +3,9 @@ package bl4ckscor3.mod.easterrabbits;
 import java.util.HashMap;
 import java.util.Random;
 
-import net.minecraft.entity.passive.RabbitEntity;
-import net.minecraft.item.Items;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.world.entity.animal.Rabbit;
+import net.minecraft.world.item.Items;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraftforge.event.TickEvent.ServerTickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -17,18 +17,18 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber
 public class EasterRabbits
 {
-	public static final HashMap<RabbitEntity,Integer> TIME_UNTIL_NEXT_EGG = new HashMap<>();
+	public static final HashMap<Rabbit,Integer> TIME_UNTIL_NEXT_EGG = new HashMap<>();
 	public static final Random RAND = new Random();
 	private static final int FREQUENCY = 6000;
 
 	@SubscribeEvent
 	public static void onEntityJoinWorld(EntityJoinWorldEvent event)
 	{
-		if(event.getEntity() instanceof RabbitEntity)
+		if(event.getEntity() instanceof Rabbit)
 		{
 			if(!TIME_UNTIL_NEXT_EGG.containsKey(event.getEntity()))
 			{
-				TIME_UNTIL_NEXT_EGG.put((RabbitEntity)event.getEntity(), RAND.nextInt(FREQUENCY) + FREQUENCY);
+				TIME_UNTIL_NEXT_EGG.put((Rabbit)event.getEntity(), RAND.nextInt(FREQUENCY) + FREQUENCY);
 				return;
 			}
 		}
@@ -37,14 +37,14 @@ public class EasterRabbits
 	@SubscribeEvent
 	public static void onLivingDeath(LivingDeathEvent event)
 	{
-		if(event.getEntity() instanceof RabbitEntity)
+		if(event.getEntity() instanceof Rabbit)
 			TIME_UNTIL_NEXT_EGG.remove(event.getEntity());
 	}
 
 	@SubscribeEvent
 	public static void onServerTick(ServerTickEvent event)
 	{
-		for(RabbitEntity rabbit : TIME_UNTIL_NEXT_EGG.keySet())
+		for(Rabbit rabbit : TIME_UNTIL_NEXT_EGG.keySet())
 		{
 			TIME_UNTIL_NEXT_EGG.put(rabbit, TIME_UNTIL_NEXT_EGG.get(rabbit) - 1);
 
