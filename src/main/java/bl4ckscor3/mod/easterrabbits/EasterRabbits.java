e68d693c47d00be91e7c289e85ebde24fa8ea22e
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-01 11:02:41 +0200

    port to 1.17.1

--- src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
+++ src/main/java/bl4ckscor3/mod/easterrabbits/EasterRabbits.java
@@ -3,12 +3,14 @@ package bl4ckscor3.mod.easterrabbits;
 import java.util.HashMap;
 import java.util.Random;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Rabbit;
 import net.minecraft.world.item.Items;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraftforge.event.TickEvent.ServerTickEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -24,11 +26,22 @@ public class EasterRabbits
 	@SubscribeEvent
 	public static void onEntityJoinWorld(EntityJoinWorldEvent event)
 	{
-		if(event.getEntity() instanceof Rabbit)
+		tryAddRabbit(event.getEntity());
+	}
+
+	@SubscribeEvent
+	public static void onLivingSpawn(LivingSpawnEvent event)
+	{
+		tryAddRabbit(event.getEntity());
+	}
+
+	private static void tryAddRabbit(Entity entity)
+	{
+		if(entity instanceof Rabbit rabbit)
 		{
-			if(!TIME_UNTIL_NEXT_EGG.containsKey(event.getEntity()))
+			if(!TIME_UNTIL_NEXT_EGG.containsKey(rabbit))
 			{
-				TIME_UNTIL_NEXT_EGG.put((Rabbit)event.getEntity(), RAND.nextInt(FREQUENCY) + FREQUENCY);
+				TIME_UNTIL_NEXT_EGG.put(rabbit, RAND.nextInt(FREQUENCY) + FREQUENCY);
 				return;
 			}
 		}
@@ -37,8 +50,8 @@ public class EasterRabbits
 	@SubscribeEvent
 	public static void onLivingDeath(LivingDeathEvent event)
 	{
-		if(event.getEntity() instanceof Rabbit)
-			TIME_UNTIL_NEXT_EGG.remove(event.getEntity());
+		if(event.getEntity() instanceof Rabbit rabbit)
+			TIME_UNTIL_NEXT_EGG.remove(rabbit);
 	}
 
 	@SubscribeEvent
@@ -48,7 +61,7 @@ public class EasterRabbits
 		{
 			TIME_UNTIL_NEXT_EGG.put(rabbit, TIME_UNTIL_NEXT_EGG.get(rabbit) - 1);
 
-			if(!rabbit.isBaby() && TIME_UNTIL_NEXT_EGG.get(rabbit) <= 0)
+			if(!rabbit.isBaby() && rabbit.isAlive() && TIME_UNTIL_NEXT_EGG.get(rabbit) <= 0)
 			{
 				rabbit.playSound(SoundEvents.CHICKEN_EGG, 1.0F, (RAND.nextFloat() - RAND.nextFloat()) * 0.2F + 1.0F);
 				rabbit.spawnAtLocation(Items.EGG, 1);
